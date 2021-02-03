d8e6d2e53dff7ded838833342a6aa55427baa501
Dalton Wilkins <wilkins.271@osu.edu>
2021-02-02 19:43:08 -0500

    pushing unit 3 material

---
+++ 03-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,8 @@
+// Write Your JavaScript Code Here
+let myName = "Dalton Wilkins";
+let pets = 0;
+let funFact = "I like anchovies";
+
+console.log("My name is " + myName);
+console.log("I have " + pets + " pets");
+console.log("Fun fact: " + funFact);
